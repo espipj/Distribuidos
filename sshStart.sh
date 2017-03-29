c7c1136d7402aaa7eee316840ffbcec76034ba01
pablo0910 <pablo0910@usal.es>
2017-03-29 11:22:22 +0200

    Minor change

--- sshStart.sh
+++ sshStart.sh
@@ -3,10 +3,11 @@
 # ./sshStart i0901995 172.20.2.14 172.20.2.15
 
 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war
+~/Z/Distribuidos/PractObligatoria/tomcat/bin/shutdown.sh
 ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
 
-ssh $1@$2 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
+ssh $1@$2 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/shutdown.sh && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
 
-ssh $1@$3 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
+ssh $1@$3 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/shutdown.sh && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
 
 curl -v http://localhost:8080/Distribuidos/despachador/inicializar?maquina=0\&ip2=$2\&ip3=$3
