42218b38affb5d4f4706ef0bb61942708066a06f
pablo0910 <pablo0910@usal.es>
2017-03-29 11:01:51 +0200

    Start script updated

--- sshStart.sh
+++ sshStart.sh
@@ -3,4 +3,8 @@
 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war
 ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
 
-curl -v http://localhost:8080/Distribuidos/despachador/inicializar?maquina=0&ip2=$1&ip3=$2
+ssh $1@$2 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
+
+ssh $1@$3 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
+
+curl -v http://localhost:8080/Distribuidos/despachador/inicializar?maquina=0&ip2=$2&ip3=$3
