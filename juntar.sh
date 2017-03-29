12d36b12aade54e71ff60f7196437587398f46f8
pablo0910 <pablo0910@usal.es>
2017-03-29 11:20:28 +0200

    Bugs scripts

--- juntar.sh
+++ juntar.sh
@@ -1,3 +1,4 @@
-cat 0.log 1.log 2.log > total.log
+cat ~/Z/Distribuidos/PractObligatoria/tiempos/0.log ~/Z/Distribuidos/PractObligatoria/tiempos/1.log ~/Z/Distribuidos/PractObligatoria/tiempos/2.log > total.log
 sort -k 3 total.log > totalSorted.log
+javac Comprobador.java
 java Comprobador totalSorted.log $1 $2
--- sshStart.sh
+++ sshStart.sh
@@ -9,4 +9,4 @@ ssh $1@$2 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Dist
 
 ssh $1@$3 cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
 
-curl -v http://localhost:8080/Distribuidos/despachador/inicializar?maquina=0&ip2=$2&ip3=$3
+curl -v http://localhost:8080/Distribuidos/despachador/inicializar?maquina=0\&ip2=$2\&ip3=$3
