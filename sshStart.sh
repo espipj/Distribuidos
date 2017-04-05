82c53264cca70d8271d6e5ce3719c8aea2a9dd0e
espipj <espipj@gmail.com>
2017-04-05 10:34:33 +0200

    Changes in sshStart

--- sshStart.sh
+++ sshStart.sh
@@ -14,13 +14,13 @@ sleep 5
 # We clean OLD logs
 rm -r $tomcat/logs/*
 # We copy the new War to the shared folder
-cp -R $projectDir$warDir $tomcat/webapps/Distribuidos.war
+cp $projectDir$warDir $tomcat/webapps/Distribuidos.war
 # We run tomcat in main machine
 "$tomcat/bin/startup.sh"
 
 # We copy tomcat server to local machine, we shutdown the server and we run it again
-ssh $1@$3 mkdir /home/$1/tomcat && cp -R $tomcat/* /home/$1/tomcat && "$tomcat/bin/shutdown.sh" && sleep 5 && "$tomcat/bin/startup.sh"
-ssh $1@$4 mkdir /home/$1/tomcat && cp -R $tomcat/* /home/$1/tomcat && "$tomcat/bin/shutdown.sh" && sleep 5 && "$tomcat/bin/startup.sh"
+ssh $1@$3 mkdir /home/$1/tomcat && cp -a $tomcat/. /home/$1/tomcat/ && "$tomcat/bin/shutdown.sh" && sleep 5 && "$tomcat/bin/startup.sh"
+ssh $1@$4 mkdir /home/$1/tomcat && cp -a $tomcat/. /home/$1/tomcat/ && "$tomcat/bin/shutdown.sh" && sleep 5 && "$tomcat/bin/startup.sh"
 
 # We launch our program in main server
 sleep 1
