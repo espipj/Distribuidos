cebd04ada6614038d8957379486b00016316d30c
espipj <espipj@gmail.com>
2017-04-05 20:17:30 +0200

    Minor fixes

--- juntar.sh
+++ juntar.sh
@@ -1,4 +1,8 @@
 cat ~/Z/Distribuidos/PractObligatoria/tiempos/0.log ~/Z/Distribuidos/PractObligatoria/tiempos/1.log ~/Z/Distribuidos/PractObligatoria/tiempos/2.log > ../tiempos/total.log
 sort -k 3 ../tiempos/total.log > ../tiempos/totalSorted.log
 javac Comprobador.java
+echo $1
+echo $2
+$1 > ../tiempos/d1.txt
+$2 > ../tiempos/d2.txt
 java Comprobador totalSorted.log $1 $2 > ../tiempos/resultado.txt
