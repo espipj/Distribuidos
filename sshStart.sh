724d0becc39e21f6da62d3d90591bee777476b0d
espipj <espipj@gmail.com>
2017-04-05 10:14:04 +0200

    Changes in sshStart

--- sshStart.sh
+++ sshStart.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-# ./sshStart i0901995 172.20.2.14 172.20.2.15
-~/Z/Distribuidos/PractObligatoria/tomcat/bin/shutdown.sh
+# ./sshStart i0901995 172.20.2.111 172.20.2.14 172.20.2.15
+
+projectDir="/home/$1/Z/Distribuidos/PractObligatoria/"
+warDir="Distribuidos/Distribuidos/Distribuidos.war"
+tomcatDir="tomcat"
+tomcat=$projectDir$tomcatDir
+
+# Shutdown Tomcat server in main machine
+"$tomcat/bin/shutdown.sh"
 sleep 5
-cp -R ~/Z/Distribuidos/PractObligatoria/Distribuidos/Distribuidos/Distribuidos.war ~/Z/Distribuidos/PractObligatoria/tomcat/webapps/Distribuidos.war
-~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
+# We clean OLD logs
+rm -r "$tomcat/logs/*"
+# We copy the new War to the shared folder
+cp -R "$projectDirDistribuidos$warDir" "$tomcat/webapps/Distribuidos.war"
+# We run tomcat in main machine
+"$tomcat/bin/startup.sh"
 
-ssh $1@$3 ~/Z/Distribuidos/PractObligatoria/tomcat/bin/shutdown.sh && sleep 5 && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
+# We copy tomcat server to local machine, we shutdown the server and we run it again
+ssh $1@$3 mkdir /home/$1/tomcat && cp -R "$tomcat/*" "/home/$1/tomcat/" && "$tomcat/bin/shutdown.sh" && sleep 5 && "$tomcat/bin/startup.sh"
+ssh $1@$4 mkdir /home/$1/tomcat && cp -R "$tomcat/*" "/home/$1/tomcat/" && "$tomcat/bin/shutdown.sh" && sleep 5 && "$tomcat/bin/startup.sh"
 
-ssh $1@$4 ~/Z/Distribuidos/PractObligatoria/tomcat/bin/shutdown.sh && sleep 5 && ~/Z/Distribuidos/PractObligatoria/tomcat/bin/startup.sh
-
+# We launch our program in main server
+sleep 1
 curl -v http://localhost:8080/Distribuidos/despachador/inicializar?maquina=0\&ip1=$2\&ip2=$3\&ip3=$4
