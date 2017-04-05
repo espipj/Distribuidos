b377acffb47149f0bf567d5e86b25e356a330a29
espipj <espipj@gmail.com>
2017-04-05 13:21:28 +0200

    Changes in Finalizador.java

--- juntar.sh
+++ juntar.sh
@@ -1,4 +1,4 @@
 cat ~/Z/Distribuidos/PractObligatoria/tiempos/0.log ~/Z/Distribuidos/PractObligatoria/tiempos/1.log ~/Z/Distribuidos/PractObligatoria/tiempos/2.log > total.log
 sort -k 3 total.log > totalSorted.log
 javac Comprobador.java
-java Comprobador totalSorted.log $1 $2
+java Comprobador totalSorted.log $1 $2 > resultado.txt
