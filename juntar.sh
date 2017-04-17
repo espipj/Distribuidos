e004484708d37b4252b3cd296ac2b18a074da226
espipj <espipj@gmail.com>
2017-04-17 19:23:38 +0200

    changes in juntar.sh

--- juntar.sh
+++ juntar.sh
@@ -3,4 +3,4 @@ sort -k 3 ~/tiempos/total.log > ~/tiempos/totalSorted.log
 javac Comprobador.java -d ~/tiempos/
 echo $1
 echo $2
-java ~/tiempos/Comprobador ~/tiempos/totalSorted.log $1 $2 > ~/tiempos/resultado.txt
+java ~/tiempos/ricart/Comprobador ~/tiempos/totalSorted.log $1 $2 > ~/tiempos/resultado.txt
