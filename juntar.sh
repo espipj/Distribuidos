393d38b75e93765ec66f5cf94835e3c4d2a3bfc4
espipj <espipj@gmail.com>
2017-04-17 19:35:17 +0200

    changes in comprobador.java

--- juntar.sh
+++ juntar.sh
@@ -1,6 +1,6 @@
 cat ~/tiempos/0.log ~/tiempos/1.log ~/tiempos/2.log > ~/tiempos/total.log
 sort -k 3 ~/tiempos/total.log > ~/tiempos/totalSorted.log
-javac Comprobador.java -d ~/tiempos/
+javac ricart/Comprobador.java
 echo $1
 echo $2
-java ~/tiempos/ricart/Comprobador ~/tiempos/totalSorted.log $1 $2 > ~/tiempos/resultado.txt
+java ricart/Comprobador ~/tiempos/totalSorted.log $1 $2 > ~/tiempos/resultado.txt
